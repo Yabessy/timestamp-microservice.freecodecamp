edcaa9b9a5fc24d9ec409c7e770aade857d24387
Replit user <>
2023-04-15 13:25:40 +0700

    upload for certification

--- index.js
+++ index.js
@@ -24,6 +24,59 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get("/api/:date", function (req, res) {
+  function isValidDateFormat(dateString) {
+    // Create a regular expression pattern to match against the Y-m-d format
+    const pattern = /^\d{4}-\d{2}-\d{2}$/;
+
+    // Test if the string matches the pattern
+    return pattern.test(dateString);
+  }
+  function convertUnixToYMD(unixTimestamp) {
+    // Create a new Date object with the Unix timestamp in milliseconds
+    const dateObj = new Date(unixTimestamp * 1);
+  
+    // Get the year, month (0-indexed), and day from the date object
+    const year = dateObj.getFullYear();
+    const month = dateObj.getMonth() + 1; // add 1 to get the month in the range 1-12
+    const day = dateObj.getDate();
+  
+    // Convert the values to strings and pad with leading zeros if necessary
+    const yearStr = year.toString();
+    const monthStr = month.toString().padStart(2, "0");
+    const dayStr = day.toString().padStart(2, "0");
+  
+    // Combine the values into a Y-m-d string
+    return `${yearStr}-${monthStr}-${dayStr}`;
+  }
+  function convertYMDToDate(ymdString) {
+    // Create a new Date object with the Y-m-d string
+    const dateObj = new Date(ymdString);
+  
+    // Get the day of the week, day of the month, month (0-indexed), and year from the   date object
+    const daysOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+    const dayOfWeek = daysOfWeek[dateObj.getUTCDay()];
+    const dayOfMonth = dateObj.getUTCDate();
+    const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep",         "Oct", "Nov", "Dec"];
+    const month = months[dateObj.getUTCMonth()];
+    const year = dateObj.getUTCFullYear();
+  
+    // Combine the values into a formatted string
+    return `${dayOfWeek}, ${dayOfMonth} ${month} ${year} 00:00:00 GMT`;
+  }
+  try{
+    if(isValidDateFormat(req.params.date)){ 
+      const theUnix = new Date(req.params.date).getTime()
+      const theUtc = convertYMDToDate(req.params.date)
+      res.json({"unix": theUnix,"utc":theUtc})
+    } else {
+      const theUtc = convertYMDToDate(convertUnixToYMD(req.params.date))
+      res.json({"unix": req.params.date, "utc": theUtc })
+    }
+  } catch(e){
+    res.json({"error": e});
+  }
+});
 
 
 // listen for requests :)
